d7a50372e6f14754eaafcefb5e8b3e36febfc7d3
Ashma-Garg <ashmagarg77u@gmail.com>
2024-04-03 19:05:02 -0400

    Web crawling permission added

--- src/main/java/com/acc/property/WebCrawler.java
+++ src/main/java/com/acc/property/WebCrawler.java
@@ -22,8 +22,7 @@ public class WebCrawler {
 	static JSONArray jsonArray = new JSONArray();
 	static WebDriverWait wait;
 
-	public static void main(String[] args)
-	{
+	public static void main() {
 		setupWebDriver();
 		startCrawling();
 	}
